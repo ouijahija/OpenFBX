1cdd4b2f0e17ae12147122b1a8127d64c29d24cb
Fredrik Vestergaard <fredrik.vestergaard@zhdk.ch>
2021-12-21 21:01:36 +0100

    Project setup

---
+++ premake5.lua
@@ -0,0 +1,26 @@
+project "OpenFBX"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/*.h",
+		"src/*.c",
+		"src/*.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
